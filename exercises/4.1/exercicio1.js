6a0783d03ab21e35038ad9fe38cccc3915bf262f
Lucas-Werner <lucas.1werner@gmail.com>
2019-11-19 17:57:47 -0300

    exercicio 1

---
+++ exercises/4.1/exercicio1.js
@@ -0,0 +1,10 @@
+let a = 5;
+let b = 10;
+ 
+const soma = a + b;
+const subtracao = b - a ;
+const multiplicacao = a * b;
+const divisao = b / a ;
+const modulo = a % 2 ;
+
+console.log (soma, subtracao, multiplicao, modulo)
