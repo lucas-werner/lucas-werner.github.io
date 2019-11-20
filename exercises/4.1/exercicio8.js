e8a5edbafdcc70819b6a5395af7d0358d91c2c38
Lucas-Werner <lucas.1werner@gmail.com>
2019-11-20 12:13:06 -0300

    exercicio 8

---
+++ exercises/4.1/exercicio8.js
@@ -0,0 +1,13 @@
+let num1 = 3
+let num2 = 8
+let num3 = 3
+
+if (num1 %2 == 0) {
+    console.log(true);
+} else if (num2 %2 == 0) {
+    console.log(true);
+} else if (num3 %2 ==0) {
+    console.log(true)
+} else {
+    console.log(false)
+}
