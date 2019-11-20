fa78f0d479509404ef7fa1784d9190ee41734df8
Lucas-Werner <lucas.1werner@gmail.com>
2019-11-20 11:14:09 -0300

    mensagem error exercicio 5

--- exercises/4.1/exercicio5.js
+++ exercises/4.1/exercicio5.js
@@ -1,10 +1,12 @@
-let AB = 60;
-let BC = 40;
+let AB = 12;
+let BC = 51;
 let AC = 60;
 
-    if (AB + BC + AC == 180) {
-    console.log(true)
-   } else {
+if (AB + BC + AC == 180) {
+    console.log(true);
+} else if (AB, BC, AC = null) { 
+    console.log("error");
+} else {
     console.log(false)
    }
  
