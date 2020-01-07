1bccb6456611e2cd101b74ebec778fde72d8c8f2
Lucas-Werner <lucas.1werner@gmail.com>
2020-01-07 18:57:48 -0300

    cabeça deu nó no factorialis

--- exercises/8.1/exercises.js
+++ exercises/8.1/exercises.js
@@ -5,3 +5,15 @@ function factorial(n) {
     }
     return result;
 }
+
+console.log(factorial(5));
+
+// recursivo
+
+function factorial(n) {
+  if (n <= 1) {
+      return 1;
+  } else {
+      return n * factorial(n - 1);
+  }
+}
