74fb925133c82b6c1bba266a5fdd07d1544eac1b
Lucas-Werner <lucas.1werner@gmail.com>
2020-01-08 15:13:38 -0300

    ache a maior palavra

--- exercises/8.1/exercises.js
+++ exercises/8.1/exercises.js
@@ -1,19 +1,29 @@
 function factorial(n) {
-    var result = 1;
-    for (let i = n; i > 1; i--) {
-        result *= i;
+    for (let i = n-1; i > 1; i--) {
+        n *= i;
     }
-    return result;
+    return n;
 }
 
 console.log(factorial(5));
 
 // recursivo
 
-function factorial(n) {
+function factorial2(n) {
   if (n <= 1) {
       return 1;
   } else {
-      return n * factorial(n - 1);
+      return n * factorial2(n - 1);
   }
 }
+
+// maior palavra, também não entendi
+
+function longestWord(str){
+  let longestWord = str.split(' ').sort(function(a, b) { return b.length - a.length; });
+  return longestWord[0];
+}
+
+console.log(longestWord("Antônio foi no banheiro e não sabemos o que aconteceu"))
+
+//
