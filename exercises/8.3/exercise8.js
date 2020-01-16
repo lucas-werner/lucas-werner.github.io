091665ba7587346760e738352766764cd5a9f120
Lucas-Werner <lucas.1werner@gmail.com>
2020-01-16 15:01:00 -0300

    ok

--- exercises/8.3/exercise8.js
+++ exercises/8.3/exercise8.js
@@ -4,7 +4,10 @@ const assert = require('assert')
 
 const greet = (name, greet = "Hi") => `${greet} ${name}`
 
+
 assert.equal(greet("John"), "Hi John")
 assert.equal(greet("John", "Good morning"), "Good morning John")
 assert.equal(greet("Isabela", "Oi"), "Oi Isabela")
 
+
+console.table("oi", "tudo bem")
