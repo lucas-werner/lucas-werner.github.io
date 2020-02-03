a35f3f765ad3ae51adfc3cc405de84a118d23b5d
Lucas-Werner <lucas.1werner@gmail.com>
2020-02-03 19:38:44 -0300

    igual gabarito

--- exercises/11.2/my-pokedex/src/App.js
+++ exercises/11.2/my-pokedex/src/App.js
@@ -2,16 +2,14 @@ import React from 'react';
 import './App.css';
 import './pokemon.css'
 import pokemons from './data'
-import Pokedex from './pokedex'
+import Pokedex from './Pokedex'
 
 function App() {
   return (
-  <div className="App">
-    <h1>Pokedex!</h1>
-    <Pokedex pokemons={pokemons} />
-  
-  </div>
-  
+    <div className="App">
+      <h1>Pokedex!</h1>
+      <Pokedex pokemons={pokemons} />
+    </div>
   )
 }
 
