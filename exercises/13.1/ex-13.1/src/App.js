28f62895d0598b52296f42d71135ad68d10d3cc7
Lucas-Werner <lucas.1werner@gmail.com>
2020-02-10 18:49:02 -0300

    exercicios 13.2

--- exercises/13.1/ex-13.1/src/App.js
+++ exercises/13.1/ex-13.1/src/App.js
@@ -1,43 +1,20 @@
-import React, { Component } from 'react';
-import Picture from './Picture';
-import Button from './Button';
-class App extends Component {
-  constructor(props) {
-    super(props);
-this.state = {
-      pictures: [
-        {id: 1, src: 'http://via.placeholder.com/200x100'},
-        {id: 2, src: 'http://via.placeholder.com/400x200'},
-        {id: 3, src: 'http://via.placeholder.com/200x100'}
-      ],
-      currentPic: null
-    };
-this.setCurrentPic = this.setCurrentPic.bind(this);
-  }
-setCurrentPic(id) {
-    this.setState({currentPic: id});
-  }
-render () {
-    return (
-      <div>
-        <div className='squares'>
-          {this.state.pictures.map((picture) => {
-            return (
-              <Picture key={picture.id} src={picture.src}>
-                <Button
-                  pictureSrc={picture.src}
-                  setCurrentPic={this.setCurrentPic}
-                  id={picture.id}
-                />
-              </Picture>
-            )
-          })}
-        </div>
-        <div>
-          <p>Current selected picture ID is {this.state.currentPic}</p>
-        </div>
-      </div>
-    )
-  }
+import React from 'react';
+import Dropdown from './Dropdown';
+import './App.css';
+
+function App() {
+  const content = [
+    {id: 1, item: 'a'},
+    {id: 2, item: 'b'},
+    {id: 3, item: 'c'},
+    {id: 4, item: 'd'},
+  ];
+
+  return (
+    <div className="App">
+      <Dropdown content={content}>Título da lista customizado</Dropdown>
+    </div>
+  );
 }
+
 export default App;
