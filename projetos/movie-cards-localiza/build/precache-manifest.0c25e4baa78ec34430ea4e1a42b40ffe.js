b236d030208f26db3fd0cdd2ec1f7d9f4f5e4b6d
Lucas-Werner <lucas.1werner@gmail.com>
2020-02-29 21:15:07 -0300

    deploy?

---
+++ projetos/movie-cards-localiza/build/precache-manifest.0c25e4baa78ec34430ea4e1a42b40ffe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f737059a4ad0fe061df7583e7e6f544",
+    "url": "/projetos/movie-cards-localiza/index.html"
+  },
+  {
+    "revision": "6c024fdf7a2faa0b0a13",
+    "url": "/projetos/movie-cards-localiza/static/css/main.c5604e8d.chunk.css"
+  },
+  {
+    "revision": "c4f87f955bff932a2698",
+    "url": "/projetos/movie-cards-localiza/static/js/2.69e4d206.chunk.js"
+  },
+  {
+    "revision": "6c024fdf7a2faa0b0a13",
+    "url": "/projetos/movie-cards-localiza/static/js/main.2d2b6210.chunk.js"
+  },
+  {
+    "revision": "fcd78bef0a1f596f3574",
+    "url": "/projetos/movie-cards-localiza/static/js/runtime-main.d97dc65b.js"
+  }
+]);
