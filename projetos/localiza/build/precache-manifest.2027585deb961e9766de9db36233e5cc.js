5d96a34b7332a5031247a1472b41508245102005
Lucas-Werner <lucas.1werner@gmail.com>
2020-02-29 22:17:28 -0300

    ok

---
+++ projetos/localiza/build/precache-manifest.2027585deb961e9766de9db36233e5cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e76b3a77a633ad99f33ec36a35769f58",
+    "url": "/projetos/localiza/index.html"
+  },
+  {
+    "revision": "a6da365bb0641ac94cb7",
+    "url": "/projetos/localiza/static/css/main.c5604e8d.chunk.css"
+  },
+  {
+    "revision": "c4f87f955bff932a2698",
+    "url": "/projetos/localiza/static/js/2.69e4d206.chunk.js"
+  },
+  {
+    "revision": "a6da365bb0641ac94cb7",
+    "url": "/projetos/localiza/static/js/main.0d37c643.chunk.js"
+  },
+  {
+    "revision": "093b648b3aaf67e78542",
+    "url": "/projetos/localiza/static/js/runtime-main.9df75f37.js"
+  }
+]);
