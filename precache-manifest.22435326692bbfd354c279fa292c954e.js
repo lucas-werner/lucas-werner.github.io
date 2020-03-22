91b5093b9310603208619a0f57acd99bfbdd2916
Lucas-Werner <lucas.1werner@gmail.com>
2020-03-22 17:13:46 -0300

    Updates

---
+++ precache-manifest.22435326692bbfd354c279fa292c954e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2ff13ded00f4c60dce4",
+    "url": "/static/css/main.c6a15035.chunk.css"
+  },
+  {
+    "revision": "a2ff13ded00f4c60dce4",
+    "url": "/static/js/main.a2ff13de.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "24022c7f400b2162ad96f5f631dcf7cb",
+    "url": "/index.html"
+  }
+];
