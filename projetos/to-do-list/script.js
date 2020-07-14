49b03d4f498c69aaec2b4938a2d61052ed446205
Lucas Werner <lucas.1werner@gmail.com>
2020-07-13 23:57:00 -0300

    voltando projetos

---
+++ projetos/to-do-list/script.js
@@ -0,0 +1,59 @@
+let newTask = document.getElementById('newTask')
+let clear = document.getElementById('clear')
+let addTask = document.getElementById('addTask')
+let highlight = document.getElementsByClassName('highlight')
+let lista = document.getElementById('list')
+
+
+addTask.addEventListener('click', function () {
+    if (newTask.value == '') {
+        alert("Sem tarefas por hoje?");
+    } else {
+        fAddTask()
+    }
+})
+
+clear.addEventListener('click', function () {
+    while (lista.firstChild) {
+        lista.removeChild(lista.firstChild)
+    }
+})
+
+newTask.addEventListener('keypress', function (e) {
+    if (e.keyCode == 13 && newTask.value == '') {
+        alert("Sem tarefas por hoje?")
+    } else if (e.keyCode == 13) {
+        fAddTask()
+    }
+})
+
+function fAddTask() {
+    let newItem = document.createElement('li');
+    newItem.innerText = newTask.value;
+    newItem.className = 'item';
+    document.getElementById('list').appendChild(newItem);
+    document.getElementById('newTask').value = '';
+    newTask.value = null;
+    console.log(event);
+    // console.log(target) let newItem = document.createElement('li');
+    if (searchElement === null) {
+        event.target.className = 'highlight';
+    } else {
+        searchElement.className = '';
+        event.target.className = 'highlight';
+    }
+
+
+}
+newItem.addEventListener('dblclick', function (event) {
+    event.target.className = 'doneItem';
+})
+
+let done = document.getElementById('done')
+done.addEventListener('click', function () {
+    var elements = document.getElementsByClassName('doneItem');
+    while (elements.length > 0) {
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+}
+)
